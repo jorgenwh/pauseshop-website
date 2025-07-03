286eb44a6ed490dedb91032ba99981f954ef523c
Tarquinen <dannysmo@gmail.com>
2025-07-03 10:20:10 -0400

    loading not used anymore

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useExtensionData } from './useExtensionData';
 export { useDeepSearch } from './useDeepSearch';
 export { useButtonDimensions } from './useButtonDimensions';
-export { useLoadingDots } from './useLoadingDots';
 export { useProductSelection } from './useProductSelection';
